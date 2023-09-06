2750c787aac69e70a9463a7eb8b365cd1d830dbc
benjascaf <benjascaf@gmail.com>
2023-09-05 21:14:33 -0300

    Some more functionality to project object

--- src/project.js
+++ src/project.js
@@ -1,12 +1,26 @@
-export default function project () {
-    const tasksContainers = {
-        projectTasks: [],
-    } //An object literal to use as a dict and store tasks directly in
-    //the project or in one of the subsections
+export default function project() {
+  const tasksContainers = {
+    projectTasks: [],
+  }; //An object literal to use as a dict and store tasks directly in
+  //the project or in one of the subsections
 
-    const addSubjection = newSubjection => {
-        tasksContainers[newSubjection] = []
-    };
+  const addSubsection = (newSubjection) => {
+    tasksContainers[newSubjection] = [];
+  };
 
-    const getSubsectionTasks()
+  //will have to remember the task's indexes using data attributes in the DOM
+  const addTaskToSubsection = (subsection, newTaskIndex) =>
+    tasksContainers[subsection].push(newTask);
+
+  const removeTaskFromSubsection = (subsection, taskToRemoveIndex) =>
+    delete tasksContainers[subsection][taskToRemoveIndex];
+
+  const getSubsectionTasks = (subsection) => tasksContainers[subsection];
+
+  return {
+    addSubsection,
+    addTaskToSubsection,
+    getSubsectionTasks,
+    removeTaskFromSubsection,
+  };
 }
